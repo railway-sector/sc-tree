d1383feda3fd902bc939cbf8eb516b3bb1db5674
EijiGorilla <emasuz@hotmail.com>
2026-06-24 11:21:45 +0800

    updated

--- src/components/MapDisplay.tsx
+++ src/components/MapDisplay.tsx
@@ -7,9 +7,11 @@ import "@arcgis/map-components/components/arcgis-basemap-gallery";
 import "@arcgis/map-components/components/arcgis-layer-list";
 import { treeGroupLayer, alignmentGroupLayer } from "../layers";
 import type { ArcgisMap } from "@arcgis/map-components/dist/components/arcgis-map";
+import { useState } from "react";
 
 function MapDisplay() {
   const arcgisMap = document.querySelector("arcgis-map") as ArcgisMap;
+  const [_mapView, setMapView] = useState<any>();
 
   arcgisMap?.viewOnReady(() => {
     arcgisMap?.map?.add(alignmentGroupLayer);
@@ -23,6 +25,9 @@ function MapDisplay() {
       basemap="dark-gray-vector"
       zoom={10}
       center="120.99, 14.4"
+      onarcgisViewReadyChange={(event: any) => {
+        setMapView(event.target.id);
+      }}
     >
       {/* <arcgis-zoom position="top-right"></arcgis-zoom> */}
     </arcgis-map>
